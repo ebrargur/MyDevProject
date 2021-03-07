b0321e366727c3ca60e52d17c4ee7c68f3baf884
ebrargur <ebrarusa@gmail.com>
2021-03-07 11:09:56 -0500

    m

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+// write your code here
+        for (int i = 0; i <4 ; i++) {
+            System.out.println("Hebfrrrmmmrvbbhnllo batch");
+            Thread.sleep(1000);
+
+        }
+    }
+}
